031f5e8575022a2cadc7519694d97e72f98f78c3
Azmi Makarima <dev.amy.azmim@gmail.com>
2022-07-24 15:52:40 +0700

    feat: add prettier for tailwind & import

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 120,
+  semi: false,
+  plugins: [require('prettier-plugin-tailwindcss'), require('@trivago/prettier-plugin-sort-imports')],
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // src/pages/_app.tsx
-import { withTRPC } from '@trpc/next'
 import type { AppRouter } from '../server/router'
+import '../styles/globals.css'
+import { withTRPC } from '@trpc/next'
+import { SessionProvider } from 'next-auth/react'
 import type { AppType } from 'next/dist/shared/lib/utils'
 import superjson from 'superjson'
-import { SessionProvider } from 'next-auth/react'
-import '../styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
@@ -15,9 +15,8 @@ const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps } }) => 
 }
 
 const getBaseUrl = () => {
-  if (typeof window !== 'undefined') {
-    return ''
-  }
+  if (typeof window !== 'undefined') return ''
+
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
