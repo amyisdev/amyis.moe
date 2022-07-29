af6b72efb2598614c5028a8bf27f81ac6cca6fb8
Azmi Makarima <dev.amy.azmim@gmail.com>
2022-07-30 01:17:40 +0700

    fix: disable env

--- src/server/env-schema.mjs
+++ src/server/env-schema.mjs
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   // DATABASE_URL: z.string().url(),
+  // NEXTAUTH_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.optional().string().url(),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
 })
